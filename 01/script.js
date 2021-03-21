75e2671e8404c41b531c4a53875f739ce649c138
CheonJihyun <31688858+CheonJihyun@users.noreply.github.com>
2021-03-22 02:04:39 +0900

    Add files via upload

---
+++ 01/script.js
@@ -0,0 +1,6 @@
+let concept1 = document.getElementById("concept1");
+let concept2 = document.getElementById("concept2");
+let concept3 = document.getElementById("concept3");
+let concept4 = document.getElementById("concept4");
+let concept5 = document.getElementById("concept5");
+
